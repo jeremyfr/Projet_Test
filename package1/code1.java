486e248dfeefd147f299126f3386d276b202c1b3
Jeremy Fricou <jeremyfricou@gmail.com>
2013-02-28 13:53:24 +0100

    Commit du fichier modifie de debut

--- package1/code1.java
+++ package1/code1.java
@@ -2,4 +2,5 @@ public static void main(String [] args){
     System.out.println("Salut");
     System.out.println("a");
     System.out.println("tous!!");
+    System.out.println("Ajout d'une ligne de debug...");
 }
